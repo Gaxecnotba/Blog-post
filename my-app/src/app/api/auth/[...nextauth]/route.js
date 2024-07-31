cde45d3de041f5896b133181e84ed2d2235ad4f6
Gaxecnotba <sebaselprodevj12@gmail.com>
2024-07-31 11:13:33 -0400

    fixed route file, src/app/api/auth/[...nextauth]/route.js

--- my-app/src/app/api/auth/[...nextauth]/route.js
+++ my-app/src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import db from "@/lib/db";
 import bcrypt from "bcrypt";
-export const authOptions = {
+
+const handler = NextAuth({
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -11,32 +12,25 @@ export const authOptions = {
         password: { label: "Password", type: "password", placeholder: "*****" },
       },
       async authorize(credentials, req) {
-        console.log(credentials);
         const userFound = await db.user.findUnique({
           where: {
             email: credentials.email,
           },
         });
         if (!userFound) throw new Error("User not found");
-        console.log(userFound);
         const matchPassword = await bcrypt.compare(
           credentials.password,
           userFound.password
         );
         if (!matchPassword) throw new Error("Wrong password");
 
-        return {
-          id: userFound.id,
-          name: userFound.username,
-          email: userFound.email,
-        };
+        return userFound;
       },
     }),
   ],
   pages: {
     signIn: "/auth/login",
   },
-};
-const handler = NextAuth(authOptions);
+});
 
 export { handler as GET, handler as POST };
